2f0ce408979a778230bdba5a792e6b6811025839
Sanjay Nagal <sanjunagal05@gmail.com>
2019-04-27 13:03:56 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # HTML-Examples
-HTML Examples
---
+++ demo.js
@@ -0,0 +1,37 @@
+let demo = "Khushal"
+let demo_one = "Sanjay";
+let x = "1";
+function one(){
+  console.log(demo)
+
+  console.log(demo_one)
+}
+
+function two(){
+  console.log(demo_one)
+}
+one();
+
+two();
+//Parantasis
+// ()
+//curly braces
+// {}
+// Square brackets
+x = `asadjakshd
+asdasdasd
+sadasdasd
+asdasdasdasdas
+asdasdasd`
+console.log(x)
+
+    var zz = null;
+  console.log(zz)
+
+
+var someex = (() => {
+  console.log("wejagjkd")
+  return 1234
+})
+
+console.log(someex)
